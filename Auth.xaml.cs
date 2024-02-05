84271a764565677f34f10920ed7ab373b3e1cda8
FaRomik <147040323+Rommaaan@users.noreply.github.com>
2024-02-05 16:40:47 +0300

    Add files via upload

---
+++ Auth.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Homework.Helper;
+
+namespace Homework
+{
+    /// <summary>
+    /// Логика взаимодействия для Auth.xaml
+    /// </summary>
+    public partial class Auth : ContentControl
+    {
+        public Auth()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogIn_Click(object sender, RoutedEventArgs e)
+        {
+            var user = ClassDB.entity.User.Where(i => i.Login == txbLogin.Text && i.Password == psbPassword.Password).FirstOrDefault();
+
+            if (user.IdRole == 1)
+            {
+                ClassFrame.frameAuth.Content = new Admin();
+            }
+            else if (user.IdRole == 2)
+            {
+                ClassFrame.frameAuth.Content = new Userrr();
+            }
+            else if (user.IdRole == 3)
+            {
+                ClassFrame.frameAuth.Content = new Student();
+            }
+            
+        }
+    }
+}
---
+++ ClassFrame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Homework.Helper;
+
+namespace Homework
+{
+    internal class ClassFrame
+    {
+        public static Frame frameAuth;
+    }
+}
